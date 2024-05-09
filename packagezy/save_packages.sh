0ff746dc59685f4ba0c818b09e366f186e1337d4
Azzy <95762556+AzzyB@users.noreply.github.com>
2024-05-09 14:53:02 -0400

    Delete packagezy directory

--- packagezy/save_packages.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Function to save the listed packages from all sources to a text file within the same directory as the script
-function save_packages {
-    echo "$PACKAGE_MANAGER" > packages.txt
-    echo "$PACKAGES" >> packages.txt
-}
